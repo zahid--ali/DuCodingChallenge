ac5fd69f28efbaf7feeda4bfc58a66bafb900b1f
zahid khan <k.zahid@keadergroup.com>
2017-12-28 09:53:45 +0300

    Initial Commit

---
+++ app/src/main/java/com/challenge/du/ui/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.challenge.du.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.challenge.du.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
